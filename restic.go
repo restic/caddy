d755491f9a25b2d49ad4672411c068be980e2fde
Aaron <44198148+whalehub@users.noreply.github.com>
2019-07-09 17:16:28 +0200

    Update the import paths for Caddy (#7)
    Signed-off-by: Aaron <github@jadja.eu>

--- restic.go
+++ restic.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 )
 
 type ResticHandler struct {
--- setup.go
+++ setup.go
@@ -1,8 +1,8 @@
 package restic
 
 import (
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 	restserver "github.com/restic/rest-server"
 )
 
